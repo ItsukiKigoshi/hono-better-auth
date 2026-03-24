4959bf8850f0f1b5a130a7f7d2db5c1cd0fad709
Itsuki Kigoshi <itsukikigoshi@gmail.com>
2026-03-24 12:58:39 -0700

    Update auth.ts

--- apps/api/src/lib/auth.ts
+++ apps/api/src/lib/auth.ts
@@ -10,9 +10,6 @@ export const getAuth = (env: Env) => {
       schema: schema,
     }),
     secret: env.BETTER_AUTH_SECRET, 
-    advanced: {
-       cookiePrefix: "__Secure-", 
-     },
      cookie: {
        sameSite: "none", 
        secure: true,
